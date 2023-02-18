c7ceea0fdf95026c0a0ed3f91c4f616649d00417
James Tan <james@predictivehire.com>
2023-02-19 00:33:28 +1030

    contextual logging with nestjs-pino

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    this.logger.log(`this is the ${AppController.name} getHello method`);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
+import { v4 } from 'uuid';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        // add a namespace to each log
+        name: 'Demo for Contextual Logging via nestjs-pino',
+        // disable logging for response
+        autoLogging: false,
+        genReqId: (req, res) => {
+          // customized requestId
+          const id = req.headers['sapia-request-id'] || v4();
+          res.setHeader('sapia-request-id', id);
+          return id;
+        },
+        // and all the other fields of:w
+        // - https://github.com/pinojs/pino-http#api
+        // - https://github.com/pinojs/pino/blob/HEAD/docs/api.md#options-object
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
